6d67378751e1a33840156996174d188c266be0ca
vdmitrii <vdmitrii.ln@gmail.com>
2021-06-26 17:32:04 +0300

    fix: Delete unused rows

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 import uuid
 
 import requests
-import websocket
+# import websocket
 from dotenv import load_dotenv
 from requests import Request, Session
 
@@ -17,66 +17,59 @@ load_dotenv()
 ACCESS_KEY = os.getenv('ACCESS_KEY')
 ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
-# @dataclass
+
 class CoinlistApi:
-    def __init__(self, access_key: str, access_secret: str = ACCESS_SECRET):
+    def __init__(self, access_key: str = ACCESS_KEY, access_secret: str = ACCESS_SECRET):
         self.access_key = access_key
         self.access_secret = access_secret
         self.endpoint_url = 'https://trade-api.coinlist.co'
-        self.wss_url ='wss://trade-api.coinlist.co'
-        self.traider_id = ''
-        # self.ws_id = 1
-        # self._ws = None
-
-        # t = threading.Thread(target=self._start_ws)
-        # t.start()
-
+        self.wss_url = 'wss://trade-api.coinlist.co'
+        # self.traider_id = ''
 
     def get_traider_id(self):
         response = self._make_requests('GET', '/v1/accounts')
-        traider_id = response.json()['accounts'][0]['trader_id']
-        print(response.status_code)
-        return traider_id
-
+        # traider_id = response.json()['accounts'][0]['trader_id']
+        # print(response.status_code)
+        return response
 
     def get_account_summary(self):
+        traider_id = self.get_traider_id()
         response = self._make_requests('GET', f'/v1/accounts/{self.traider_id}')
         asset_balances = response.json()['asset_balances']
         asset_holds = response.json()['asset_holds']
         net_liquidation_value_usd = response.json()['net_liquidation_value_usd']
-        # print(response.status_code)
         return asset_balances, asset_holds, net_liquidation_value_usd
 
-
-    def _sign(self, message, secret):
+    def _sign(self, message: str, secret: str):
         secret = base64.b64decode(self.access_secret).strip()
         message = message.encode('utf-8')
         h = hmac.new(secret, message, digestmod=hashlib.sha256)
         signature = base64.b64encode(h.digest())
         return signature.decode('utf-8')
-    
 
-    def _make_requests(self, method='POST', path=None, data=None, timeout=10):
-        # path = path or None
-        # data = data or None
+    def _make_requests(self, method: str='POST', path: str='', data: str='', timeout: int=10):
         url = self.endpoint_url + path
-        # body = {}
+        print('url', url)
         timestamp = str(int(time.time()))
+        print('timestamp', timestamp)
         json_body = json.dumps((data), separators=(',', ':')).strip()
-        message = timestamp + method + path + ('' or json_body)
+        print('json_body', json_body)
+        message = timestamp + method + path + json_body
+        print('message', message)
         signature = self._sign(message, self.access_secret)
+        print('signature', signature)
         headers = {
             'Content-Type': 'application/json',
             'CL-ACCESS-KEY': self.access_key,
             'CL-ACCESS-SIG': signature,
             'CL-ACCESS-TIMESTAMP': timestamp
         }
+        print(headers)
         s = Session()
         req = Request(method=method, url=url, data=json_body, headers=headers)
         prepped = s.prepare_request(req)
         response = s.send(prepped, timeout=timeout)
-        return response
-
+        return response.json()
 
     def show_symbols(self):
         r = self._make_requests('GET', '/v1/symbols')
@@ -87,9 +80,7 @@ class CoinlistApi:
                 symbols.append(i['symbol'])
                 f.write(i['symbol'] + '\n')
 
-
-    def create_order(self, price, size, symbol='ICP-USD', side='sell', order_type='limit'):
-   
+    def create_order(self, price: float, size: int, symbol: str='ICP-USD', side: str='sell', order_type: str='limit'):
         data = {
             'symbol': symbol,
             'type': order_type,
@@ -98,40 +89,27 @@ class CoinlistApi:
             'price': price,
             "origin": "api"
         }
-        
         response = self._make_requests(method='POST', path='/v1/orders', data=data)
         try:
             returned = response.json()['order']['order_id']
-            
         except Exception:
                 print('Ошибка при размещении ордера: ')
-
         return returned
 
-
-    def create_orders(self, symbol):
+    def create_orders(self, symbol: str):
         response = self._make_requests('POST', '/v1/orders/bulk')
         print(response.status_code)
 
-
-    def cancel_order(self, order_id):
-        params = {
-            'order_id': str(order_id)
-        }
+    def cancel_order(self, order_id: str):
+        params = {'order_id': str(order_id)}
         res = self._make_requests('DELETE', f'/v1/orders/{order_id}', data=params)
         return res
 
-
-    def cancel_by_symbol(self, symbol='ICP-USD'):
-        headers = {
-            'accept': 'application/json'
-        }
-        data = {
-            'symbol': symbol
-        }
+    def cancel_by_symbol(self, symbol: str='ICP-USD'):
+        headers = {'accept': 'application/json'}
+        data = {'symbol': symbol}
         res = self._make_requests('DELETE', '/v1/orders', data=data)
         return res
-
 
     def cancel_all(self, uids: list=None):
         uids = uids or None
@@ -139,42 +117,30 @@ class CoinlistApi:
             'accept': 'application/json',
             'Content-Type': 'application/json'
         }
-        data = [
-            'string1($uuid)',
-            'string2($uuid)',
-            'string3($uuid)'
-        ]
-        res = self._make_requests('DELETE', '/v1/orders', data=data)
+        res = self._make_requests('DELETE', '/v1/orders', data=uids)
         return res
 
-
-    def get_orders(self, order_id):
+    def get_orders(self, order_id: str):
         data={}
         response = self._make_requests('GET', f'/v1/symbols/{order_id}', data=data)
         print(response.status_code)
 
-
-    def get_symbol(self, symbol):
+    def get_symbol(self, symbol: str):
         data={}
         response = self._make_requests('GET', f'/v1/symbols/{symbol}', data=data)
         print(response.status_code)
 
-
     def exchange_time(self):
         return None
-
 
     def fills(self):
         return None
 
-
     def orders_info(self):
         return None
 
-
     def order_info(self):
         return None
-
 
     def _uuid(self):
         return str(uuid.uuid1())
@@ -182,3 +148,6 @@ class CoinlistApi:
 
 if __name__ == '__main__':
     coinlist = CoinlistApi(ACCESS_KEY, ACCESS_SECRET)
+    print(ACCESS_KEY)
+    print(ACCESS_SECRET)
+    print(coinlist.get_traider_id())
